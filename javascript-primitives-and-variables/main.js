6610900a544f3175d7bd18805447d448aebf835f
Shawn Kost <shawnmkost@gmail.com>
2020-11-16 13:00:32 -0800

    Completed javascript-primitives-and-variables | Shawn Kost

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-console */
+var fullName = "Shawn";
+console.log("value of fullName:", fullName);
+
+var isCool = true;
+console.log("value of isCool?:", isCool);
+
+var totalPets = 3;
+console.log("value of totalPets:", totalPets);
+
+console.log("typeof fullName:", typeof fullName);
+console.log("typeof isCool:", typeof isCool);
+console.log("typeof totalPets:", typeof totalPets);
+
+var unknown = null;
+console.log("value of unknown:", unknown);
+console.log("typeof unknown:", typeof unknown);
+
+var never;
+console.log("value of never:". never);
+console.log("typeof never:", never);
