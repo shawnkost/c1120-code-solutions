1c663f7fa992492bae1684c20d7f256f043b644c
Shawn Kost <shawnmkost@gmail.com>
2020-11-25 13:07:11 -0800

    Added accuracy

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -2,18 +2,26 @@ var span = document.querySelectorAll("span");
 var text = document.querySelector("h1");
 var body = document.querySelector("body");
 var i = 0;
+var errors = 0;
 span[i].className = "active";
 body.addEventListener("keydown", characterPress);
 function characterPress(event) {
   span[i].className = "active";
   if (span[i].textContent !== event.key) {
     span[i].className = "red active";
+    errors++;
   } else {
     span[i].className = "green";
     i++;
     span[i].className = "active";
   }
-
+  if (i === span.length -1 && span[i].className == "active") {
+    var total = errors + 30;
+    var accuracy = (30 / total) * 100 ;
+    accuracy = accuracy.toFixed(2);
+    text.textContent = "Your accuracy is" + " " + accuracy + "%";
+    console.log(errors);
+  }
 }
 
 //start on first character
