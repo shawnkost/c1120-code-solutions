5f9b0cf1910dc5deb30d3cfa5544a4157f9c7f94
Shawn Kost <shawnmkost@gmail.com>
2020-11-25 15:10:25 -0800

    Completed javascript-typing-tutor | Shawn Kost

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,30 +1,32 @@
-var span = document.querySelectorAll("span");
-var text = document.querySelector("h1");
-var body = document.querySelector("body");
+var $span = document.querySelectorAll("span");
+var $text = document.querySelector("h1");
+var $body = document.querySelector("body");
+var $button = document.querySelector("button");
 var i = 0;
 var errors = 0;
-span[i].className = "active";
-body.addEventListener("keydown", characterPress);
+$span[i].className = "active";
+$body.addEventListener("keydown", characterPress);
 function characterPress(event) {
-  span[i].className = "active";
-  if (span[i].textContent !== event.key) {
-    span[i].className = "red active";
+  $span[i].className = "active";
+  if ($span[i].textContent !== event.key) {
+    $span[i].className = "red active";
     errors++;
   } else {
-    span[i].className = "green";
+    $span[i].className = "green";
     i++;
-    span[i].className = "active";
+    $span[i].className = "active";
   }
-  if (i === span.length -1 && span[i].className == "active") {
+  if (i === $span.length -1 && $span[i].className == "active") {
     var total = errors + 30;
     var accuracy = (30 / total) * 100 ;
     accuracy = accuracy.toFixed(2);
-    text.textContent = "Your accuracy is" + " " + accuracy + "%";
-    console.log(errors);
+    $text.textContent = "Your accuracy is" + " " + accuracy + "%";
+    $button.className = "show";
   }
 }
 
-//start on first character
-//if inputed correctly change the class so the letter turns green
-//if inputed incorrectly change the class so the letter turns red
-//
+function resetPage(click) {
+  location.reload();
+}
+
+$button.addEventListener("click", resetPage);
