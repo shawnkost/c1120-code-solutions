9f3f58372834ab0b5204850ff468127f0511b1d3
Shawn Kost <shawnmkost@gmail.com>
2020-11-30 16:02:24 -0800

    Updated code for javascript-view-swapping

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -3,24 +3,23 @@ var $tabList = document.querySelectorAll(".tab");
 var $viewList = document.querySelectorAll(".view");
 
 $tabContainer.addEventListener("click", function (event) {
-  if (event.target.matches(".tab")) {
+  if (!event.target.matches(".tab")) {
+    return;
+  }
     for (var i = 0; i < $tabList.length; i++) {
-      if (event.target !== $tabList[i]) {
-        $tabList[i].className = "tab";
-      } else {
+      if (event.target === $tabList[i]) {
         $tabList[i].className = "tab active";
+       } else {
+        $tabList[i].className = "tab";
       }
-      if (event.target.className == "tab") {
-        var dataView = event.target.dataset.view;
+
+        var dataView = event.target.getAttribute("data-view");
         for (var k = 0; k < $viewList.length; k++) {
-          if ($viewList[k].dataset.view !== dataView) {
-            $viewList[k].className = "hidden";
+          if ($viewList[k].getAttribute("data-view") !== dataView) {
+            $viewList[k].className = "view hidden";
           } else {
             $viewList[k].className = "view"
           }
         }
       }
-    }
-
-  }
-})
+  })
