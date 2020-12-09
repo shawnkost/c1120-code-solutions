5aacf65a9dbaadd424b36672109253b60bc31738
Shawn Kost <shawnmkost@gmail.com>
2020-12-08 16:48:51 -0800

    Completed javascript-constructors | Shawn Kost

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,38 @@
+var newObj = new Object();
+
+console.log("value of newObj:", newObj);
+
+console.log("typeof newObj:", typeof newObj);
+
+var array = new Array();
+
+console.log("value of array:", array);
+
+console.log("typeof array:", typeof array)
+
+var newFunction = new Function();
+
+console.log("value of newFunction:", newFunction);
+
+console.log("typeof newFunction:", typeof newFunction);
+
+var newNum = new Number();
+
+console.log("value of newNum:", newNum);
+
+console.log("typeof newNum:", typeof newNum);
+
+var boolean = new Boolean();
+
+console.log("value of boolean:", boolean);
+
+console.log("typeof boolean:", typeof boolean);
+
+var string = new String();
+
+console.log("value of string:", string);
+
+console.log("typeof string:", typeof string);
+
+
+var capo = new Student("Mike", "Capo", "Golang")
