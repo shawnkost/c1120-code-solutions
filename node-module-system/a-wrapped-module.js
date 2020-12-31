42f2fe2a22f038dc5788c1af42dc4405cf3074b0
shawnkost <shawnmkost@gmail.com>
2020-12-31 15:55:19 -0800

    Completed node-module-system | Shawn Kost

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log("typeof exports:", typeof exports);
+console.log("value of exports:", exports);
+
+console.log("typeof require:", typeof require);
+console.log("value of require:", require);
+
+console.log("typeof module:", typeof module);
+console.log("value of module:", module);
+
+console.log("typeof _filename:", typeof __filename);
+console.log("value of _filename:", __filename);
+
+console.log("typeof _dirname:", typeof __dirname);
+console.log("value of _dirname:", __dirname);
