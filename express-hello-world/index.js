e934331d525a690dd15f4c534592b093d559ef43
shawnkost <shawnmkost@gmail.com>
2021-01-06 13:24:09 -0800

    Removed console logs

--- express-hello-world/index.js
+++ express-hello-world/index.js
@@ -2,10 +2,8 @@ const express = require("express");
 const app = express();
 
 app.use((req, res) => {
-  console.log(req.method);
   res.send("Hello World");
 })
 
 app.listen(3000, () => {
-  console.log("Express server listening on Port 3000");
 })
