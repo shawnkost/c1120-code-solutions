8bc271e47adda371c1ae3f73cab4b32b1becb340
shawnkost <shawnmkost@gmail.com>
2021-01-08 01:37:50 -0800

    Finished put, but still need to double check all requests to make sure everything is smooth

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -47,42 +47,73 @@ app.post("/api/notes", (req, res) => {
       if (err) {
         return res.status(500).json({
           error: "An unexpected error occurred."
-        })
+        });
       }
       res.status(201).json(JSONData.notes[newID]);
-    })
+    });
   }
 })
 
 app.delete("/api/notes/:id", (req, res) => {
   const ID = Number(req.params.id);
+  if (Math.sign(ID) !== 1) {
+    return res.status(400).json({
+      error: "ID given was not a positive integer"
+    });
+  }
   if (!JSONData.notes.hasOwnProperty(ID)) {
     return res.status(404).json({
       error: `cannot find note with id ${ID}`
     });
   }
   for (const index in JSONData.notes) {
-    if (Math.sign(ID) !== 1) {
-      return res.status(400).json({
-        error: "ID given was not a positive integer"
-      });
-    }
     if (ID === Number(index)) {
       delete JSONData.notes[ID];
     }
   }
   let newData = JSONData;
   newData = JSON.stringify(newData, null, 2);
-  fs.writeFile("./yo/data.json", newData, "utf8", err => {
+  fs.writeFile("./data.json", newData, "utf8", err => {
     if (err) {
-      console.log("yo");
       return res.status(500).json({
         error: "An unexpected error occurred."
-      })
+      });
     }
     res.sendStatus(204);
   })
 })
 
+app.put("/api/notes/:id", (req, res) => {
+  const content = req.body;
+  const ID = Number(req.params.id);
+  if (Math.sign(ID) !== 1) {
+    return res.status(400).json({
+      error: "ID given was not a positive integer"
+    });
+  }
+  if (Object.entries(content).length === 0) {
+    return res.status(400).json({
+      error: "content is a required field"
+    });
+  }
+  if (!JSONData.notes.hasOwnProperty(ID)) {
+    return res.status(404).json({
+      error: `cannot find note with id ${ID}`
+    });
+  }
+
+  JSONData.notes[req.params.id] = content;
+  JSONData.notes[req.params.id].id = req.params.id
+  let newData = JSONData;
+  newData = JSON.stringify(newData, null, 2);
+  fs.writeFile("./data.json", newData, "utf8", err => {
+    if (err) {
+      return res.status(500).json({
+        error: "An unexpected error occurred"
+      });
+    }
+    res.status(200).json(content);
+  });
+})
 
 app.listen(3000, () => {});
