a5a1ab67f265435d7cb816925fbce801515745c0
shawnkost <shawnmkost@gmail.com>
2021-01-09 11:42:44 -0800

    Fixed issue with the post request not being sent to the server

--- express-notes-api/main.js
+++ express-notes-api/main.js
@@ -1,58 +1,79 @@
-const $taskList = document.querySelector(".task-list-container")
-const $inputField = document.querySelector("#task");
+const $taskList = document.querySelector(".task-list-container");
+
+window.addEventListener("DOMContentLoaded", event => {
+  loadTasks();
+})
 
 const loadTasks = () => {
-  // console.log("hello");
+  $taskList.innerHTML = "";
   const xhr = new XMLHttpRequest();
   xhr.open("GET", "http://localhost:3000/api/notes");
   xhr.responseType = "json";
   xhr.addEventListener("load", data => {
     const ul = document.createElement("ul");
     for (const index of data.target.response) {
-      // console.log(index.content);
       const li = document.createElement("li");
       const taskContent = document.createElement("div");
       const deleteIcon = document.createElement("i");
       const editIcon = document.createElement("i");
-      li.setAttribute("class", "border-bottom d-flex justify-content-center align-items-center")
-      taskContent.setAttribute("class", "pr-5")
+      li.setAttribute("class", "border-bottom d-flex justify-content-center align-items-center");
+      taskContent.setAttribute("class", "pr-5");
       taskContent.textContent = index.content;
-      deleteIcon.setAttribute("class", "fas fa-ban pr-3");
-      editIcon.setAttribute("class", "fas fa-pencil-alt")
+      deleteIcon.setAttribute("class", "fas fa-ban pr-3 delete cursor-pointer");
+      editIcon.setAttribute("class", "fas fa-pencil-alt edit cursor-pointer");
       li.appendChild(taskContent);
       li.appendChild(deleteIcon);
       li.appendChild(editIcon);
       ul.appendChild(li);
     }
     $taskList.appendChild(ul);
-  })
+  });
   xhr.addEventListener('error', error => {
     console.log('Request Error:', error);
   });
-  // console.log("input field:", $inputField);
-  xhr.send()
+  xhr.send();
 }
+
+const $inputField = document.querySelector("#task");
 
 $inputField.addEventListener("keydown", event => {
   if (event.key === "Enter") {
-    createNewTask()
+    createNewTask();
+    $inputField.value = "";
   }
 })
 
 const createNewTask = () => {
   const xhr = new XMLHttpRequest();
-  xhr.open("POST", `http://localhost:3000/api/notes`);
+  xhr.open("POST", "http://localhost:3000/api/notes");
+  xhr.setRequestHeader("Content-type", "application/json");
   xhr.responseType = "json";
-  xhr.addEventListener("load", data => {})
+  xhr.addEventListener("load", data => {
+    loadTasks();
+  });
   xhr.addEventListener('error', error => {
     console.log('Request Error:', error);
   });
-  let test = {
-    content: "yo"
-  }
-  xhr.send(JSON.stringify(test));
-  loadTasks();
+  xhr.send(JSON.stringify({content: `${$inputField.value}`}));
 }
 
+const $deleteBtn = document.querySelectorAll(".delete")
+console.log($deleteBtn)
 
-loadTasks();
+// $deleteBtn.addEventListener("click", () => {
+//   deleteTask();
+// })
+
+const deleteTask = () => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("DELETE", "http://localhost:3000/api/notes");
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.responseType = "json";
+  xhr.addEventListener("load", data => {
+    loadTasks();
+  });
+  xhr.addEventListener("error", error => {
+    console.log("Request Error:", error);
+  });
+  xhr.send(JSON.stringify())
+}
