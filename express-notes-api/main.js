1fdaaa4ac834b2064b745e26389d47dcc70ab815
shawnkost <shawnmkost@gmail.com>
2021-01-09 19:25:25 -0800

    Added front-end functionality for api

--- express-notes-api/main.js
+++ express-notes-api/main.js
@@ -10,26 +10,36 @@ const loadTasks = () => {
   xhr.open("GET", "http://localhost:3000/api/notes");
   xhr.responseType = "json";
   xhr.addEventListener("load", data => {
-    const ul = document.createElement("ul");
+    const $ul = document.createElement("ul");
     for (const index of data.target.response) {
-      const li = document.createElement("li");
-      const taskContent = document.createElement("div");
-      const deleteIcon = document.createElement("i");
-      const editIcon = document.createElement("i");
-      li.setAttribute("class", "border-bottom d-flex justify-content-center align-items-center");
-      taskContent.setAttribute("class", "pr-5");
-      taskContent.textContent = index.content;
-      deleteIcon.setAttribute("class", "fas fa-ban pr-3 delete cursor-pointer");
-      editIcon.setAttribute("class", "fas fa-pencil-alt edit cursor-pointer");
-      li.appendChild(taskContent);
-      li.appendChild(deleteIcon);
-      li.appendChild(editIcon);
-      ul.appendChild(li);
+      const $li = document.createElement("li");
+      const $input = document.createElement("input");
+      const $deleteIcon = document.createElement("i");
+      $li.setAttribute("class", "d-flex justify-content-center align-items-center");
+      $li.setAttribute("id", `${index.id}`);
+      $input.setAttribute("class", "border-bottom notes-input")
+      $input.setAttribute("type", "text");
+      $input.setAttribute("id", "notes");
+      $input.setAttribute("name", "notes");
+      $input.value = index.content;
+      $deleteIcon.setAttribute("class", "fas fa-ban pr-3 delete cursor-pointer");
+      $li.appendChild($input);
+      $li.appendChild($deleteIcon);
+      $ul.appendChild($li);
     }
-    $taskList.appendChild(ul);
-  });
-  xhr.addEventListener('error', error => {
-    console.log('Request Error:', error);
+    $taskList.appendChild($ul);
+    const $deleteBtn = document.querySelectorAll(".delete")
+    for (const i of $deleteBtn) {
+      i.addEventListener("click", event => {
+        deleteTask(event);
+      });
+    }
+    const $notesInput = document.querySelectorAll(".notes-input");
+    for (const l of $notesInput) {
+      l.addEventListener("keyup", event => {
+        editTask(event);
+      })
+    }
   });
   xhr.send();
 }
@@ -43,6 +53,13 @@ $inputField.addEventListener("keydown", event => {
   }
 })
 
+const $add = document.querySelector(".add");
+
+$add.addEventListener("click", () => {
+  createNewTask();
+  $inputField.value = "";
+})
+
 const createNewTask = () => {
   const xhr = new XMLHttpRequest();
   xhr.open("POST", "http://localhost:3000/api/notes");
@@ -51,29 +68,35 @@ const createNewTask = () => {
   xhr.addEventListener("load", data => {
     loadTasks();
   });
-  xhr.addEventListener('error', error => {
-    console.log('Request Error:', error);
-  });
-  xhr.send(JSON.stringify({content: `${$inputField.value}`}));
+  xhr.send(JSON.stringify({
+    content: `${$inputField.value}`
+  }));
 }
 
-const $deleteBtn = document.querySelectorAll(".delete")
-console.log($deleteBtn)
 
-// $deleteBtn.addEventListener("click", () => {
-//   deleteTask();
-// })
-
-const deleteTask = () => {
+const deleteTask = event => {
+  const parentID = event.target.parentElement;
   const xhr = new XMLHttpRequest();
-  xhr.open("DELETE", "http://localhost:3000/api/notes");
+  xhr.open("DELETE", `http://localhost:3000/api/notes/${parentID.getAttribute("id")}`);
+  xhr.responseType = "json";
+  xhr.addEventListener("load", data => {
+    parentID.remove()
+  });
+  xhr.send();
+}
+
+
+const editTask = event => {
+  const updatedTask = event.target.value;
+  const parentID = event.target.parentElement;
+  const xhr = new XMLHttpRequest();
+  xhr.open("PUT", `http://localhost:3000/api/notes/${parentID.getAttribute("id")}`);
   xhr.setRequestHeader("Content-type", "application/json");
   xhr.responseType = "json";
   xhr.addEventListener("load", data => {
-    loadTasks();
+
   });
-  xhr.addEventListener("error", error => {
-    console.log("Request Error:", error);
-  });
-  xhr.send(JSON.stringify())
+  xhr.send(JSON.stringify({
+    content: `${updatedTask}`
+  }));
 }
