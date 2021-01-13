f0eaf1f0fa54deaf14c664f13bc09569edd69009
shawnkost <shawnmkost@gmail.com>
2021-01-13 10:48:47 -0800

    Trying to fix syntax error for with statement

--- sql-aggregates/profit-margin.sql
+++ sql-aggregates/profit-margin.sql
@@ -1,20 +1,21 @@
 with cte_replacementCost as (
   select sum("replacementCost") as "replacementCost"
         from "films"
-        join "inventory" as "i" using ("filmId")
-        group by "filmId"
+        join "inventory" using ("filmId")
+    group by "filmId"
 )
 
 with cte_revenue as (
-  select sum("amount") as "revenue"
+  select sum("amount") as "revenue1"
         from "payments"
-        join "rentals" as "r" using ("rentalId")
+        join "rentals" using ("rentalId")
         join "inventory" using ("inventoryId")
     group by "filmId"
 )
 
 select "title",
-       sum("cost", "revenue")
+   sum("cost", "revenue")
   from "films"
   join "cte_replacementCost" as "cost" using ("filmId")
   join "cte_revenue" as "revenue" using ("filmId");
+  group by "title"
