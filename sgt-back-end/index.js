5118d6a1de61c3a65905af93d2be4ab500f75742
shawnkost <shawnmkost@gmail.com>
2021-01-13 21:31:03 -0800

    Completed sgt-back-end | Shawn Kost

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -96,13 +96,11 @@ app.put('/api/grades/:gradeId', (req, res) => {
   db.query(sql, [values.name, values.course, values.score, gradeId])
     .then(result => {
       const grade = result.rows;
-      console.log("grade", grade);
       if (grade.length === 0) {
         res.status(404).json({
-          error: "Test"
-        })
+          error: "gradeId does not exist in the database"
+        });
       } else {
-        console.log("grade", grade);
         res.status(200).json(grade[0]);
       }
     })
@@ -113,47 +111,31 @@ app.put('/api/grades/:gradeId', (req, res) => {
     });
 })
 
-
-
-
-// app.put('/api/grades/:gradeId', (req, res) => {
-//   const values = req.body;
-//   const scoreInt = parseInt(values.score, 10);
-//   const gradeId = Number(req.params.gradeId);
-//   if (!Number.isInteger(gradeId) || gradeId <= 0) {
-//     res.status(400).json({
-//       error: "Invalid gradeId"
-//     });
-//     return;
-//   }
-//   if (Object.entries(values).length !== 3) {
-//     res.status(400).json({
-//       error: "Name, course, & score are required properties"
-//     });
-//     return;
-//   }
-//   if (scoreInt < 1 || scoreInt > 100) {
-//     res.status(400).json({
-//       error: "The score must be an integer between 1-100"
-//     });
-//     return;
-//   }
-//   const sql = `
-//     update "grades"
-//     set "name" = $1,
-//     "course" = $2,
-//     "score" = $3
-//     where "gradeId" = $4
-//     returning *;`;
-//   db.query(sql, [values.name, values.course, values.score, gradeId])
-//     .then(result => {
-//       const grade = result.rows;
-//       console.log("grade", grade);
-//       res.status(200).json(grade[0]);
-//     })
-//     .catch(err => {
-//       res.status(500).json({
-//         error: "Database query was not successful"
-//       });
-//     });
-// })
+app.delete('/api/grades/:gradeId', (req, res) => {
+  const  gradeId = Number(req.params.gradeId);
+  if (!Number.isInteger(gradeId) || gradeId <= 0) {
+    res.status(400).json({
+      error: "Invalid gradeId"
+    });
+    return;
+  }
+  const sql = `
+    delete from "grades"
+    where "gradeId" = $1
+    returning *;`;
+  db.query(sql, [gradeId])
+  .then(result => {
+    const grade = result.rows;
+    if (grade.length === 0) {
+      res.status(404).json({
+        error: "gradeId does not exist in the database"
+      });
+    }
+    res.status(200).json(grade[0])
+  })
+  .catch(err => {
+    res.status(500).json({
+      error: "Database query was not successful"
+    });
+  });
+})
