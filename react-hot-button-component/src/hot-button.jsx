0e2bb8aed927075fc7f9e971be9104d1468e43b5
shawnkost <shawnmkost@gmail.com>
2021-01-19 14:49:35 -0800

    Updated changes from feedback

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -5,36 +5,40 @@ export default class CustomButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = { count: 1 };
-    this.changeColor.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.changeColor = this.changeColor.bind(this);
   }
   handleClick() {
     this.setState({ count: this.state.count + 1 });
     this.changeColor();
   }
-  changeColor(props) {
-    if (this.state.count === 3) {
-      this.props.class = 'threeClicks';
+  changeColor() {
+    let newClass = " ";
+    if (this.state.count > 3) {
+      newClass = 'threeClicks';
     }
-    if (this.state.count === 6) {
-      this.props.class = 'sixClicks';
+    if (this.state.count > 6) {
+      newClass = 'sixClicks';
     }
-    if (this.state.count === 9) {
-      this.props.class = 'nineClicks';
+    if (this.state.count > 9) {
+      newClass = 'nineClicks';
     }
-    if (this.state.count === 12) {
-      this.props.class = 'twelveClicks';
+    if (this.state.count > 12) {
+      newClass = 'twelveClicks';
     }
-    if (this.state.count === 15) {
-      this.props.class = 'fifteenClicks';
+    if (this.state.count > 15) {
+      newClass = 'fifteenClicks';
     }
-    if (this.state.count === 18) {
-      this.props.class = 'eighteenClicks';
+    if (this.state.count > 18) {
+      newClass = 'eighteenClicks';
     }
+    return newClass;
   }
   render() {
+    let newClass = this.changeColor();
     return (
       <div>
-        <button onClick={this.handleClick.bind(this)} className={this.props.class}>Click Me!</button>
+        <button onClick={this.handleClick} className={newClass}>Click Me!</button>
       </div>
     )
   }
