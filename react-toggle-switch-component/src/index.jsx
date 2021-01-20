ff3594cdc9e559f512eebc0c481c02d970ac70ca
shawnkost <shawnmkost@gmail.com>
2021-01-19 17:46:23 -0800

    Completed react-toggle-switch-component | Shawn Kost

---
+++ react-toggle-switch-component/src/index.jsx
@@ -0,0 +1,8 @@
+import ToggleSwitch from "./toggle-switch"
+import React from "react";
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+  <ToggleSwitch />,
+  document.querySelector("#root")
+);
