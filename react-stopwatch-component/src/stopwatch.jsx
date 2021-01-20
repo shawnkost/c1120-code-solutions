587c8e86de6e49616bff47da4ae2d85c3af41203
shawnkost <shawnmkost@gmail.com>
2021-01-20 10:36:34 -0800

    Added changes from feedback

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -4,52 +4,44 @@ export default class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      timerOn: false,
       timerTime: 0,
-      icon: "fas fa-play",
-      intervalId: ""
+      intervalId: null
     }
+    this.icon = ""
     this.playbackSwitch = this.playbackSwitch.bind(this);
     this.startWatch = this.startWatch.bind(this);
     this.updateTime = this.updateTime.bind(this);
     this.resetWatch = this.resetWatch.bind(this);
   }
   playbackSwitch() {
-    if (this.state.timerOn === true) {
+    if (this.state.intervalId !== null) {
       clearInterval(this.state.intervalId);
-      this.setState({
-        timerOn: false,
-        icon: "fas fa-play",
-        intervalId: ""
-      });
+      this.setState({ intervalId: null });
       return;
     }
-    this.setState({
-      timerOn: true,
-      icon: "fas fa-pause"
-    });
     this.startWatch();
   }
   startWatch() {
     const interval = setInterval(this.updateTime, 1000);
-    this.setState({intervalId: interval});
+    this.setState({ intervalId: interval });
   }
   updateTime() {
-    this.setState({
-      timerTime: this.state.timerTime + 1
-    })
+    this.setState({ timerTime: this.state.timerTime + 1 })
   }
   resetWatch() {
-    if (this.state.timerOn === false)
-    this.setState({
-      timerTime: 0
-    })
+    if (this.state.intervalId === null)
+    this.setState({ timerTime: 0 })
   }
   render() {
+    if (this.state.intervalId !== null) {
+      this.icon = "fas fa-pause"
+    } else {
+      this.icon = "fas fa-play";
+    }
     return (
       <div className="margin-left">
-        <div className="time-div" onClick={this.resetWatch}>{this.state.timerTime}</div>
-        <i className={this.state.icon} onClick={this.playbackSwitch}></i>
+        <div className="time-div" onClick={ this.resetWatch }>{ this.state.timerTime }</div>
+        <i className={ this.icon } onClick={ this.playbackSwitch }></i>
       </div>
     )
   }
