741d8d8bcf5be83978c1f2a8d9a721f9fd28615c
shawnkost <shawnmkost@gmail.com>
2021-01-21 16:33:37 -0800

    Updated code from feedback

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -3,39 +3,56 @@ import React from "react";
 export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { accordionOpen: false };
-    this.test = this.test.bind(this);
+    this.state = {
+      accordionOpen: false,
+      openId: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
-  test(event) {
-    if (this.state.accordionOpen === false) {
-      this.setState({ accordionOpen: true} );
-      return event.target.nextSibling.className = "show";
-    } else if (event.target.nextSibling.className === "show") {
-      this.setState({ accordionOpen: false });
-      const details = document.querySelector(".show");
-      details.className = "hide";
+  handleClick(id) {
+    if (
+      this.state.accordionOpen === false ||
+      (this.state.accordionOpen === true &&
+        id !== 0 &&
+        id !== this.state.openId)
+    ) {
+      this.setState({
+        accordionOpen: true,
+        openId: id,
+      });
     } else {
-      this.setState({ accordionOpen: true });
-      const details = document.querySelector(".show");
-      details.className = "hide";
-      return (event.target.nextSibling.className = "show");
+      this.setState({
+        accordionOpen: false,
+        openId: 0,
+      });
     }
   }
   render() {
     return (
       <div className="container">
-        <Topics topics={this.props.topics} test={this.test} />
+        <Topics
+          topics={this.props.topics}
+          handleClick={this.handleClick}
+          openId={this.state.openId}
+        />
       </div>
     );
   }
 }
 
 function TopicDetails(props) {
-  return <div className="hide">{props.value.details}</div>
+  if (props.openId === props.value.id) {
+    return <div className="show">{props.value.details}</div>;
+  }
+  return <div className="hide">{props.value.details}</div>;
 }
 
 function TopicName(props) {
-  return <div className="topics" onClick={props.test}>{props.value.name}</div>
+  return (
+    <div className="topics" onClick={props.handleClick}>
+      {props.value.name}
+    </div>
+  );
 }
 
 function Topics(props) {
@@ -43,8 +60,12 @@ function Topics(props) {
   const newTopics = topics.map((topic) => {
     return (
       <div>
-        <TopicName key={topic.name} value={topic} test={props.test} />
-        <TopicDetails value={topic} />
+        <TopicName
+          key={topic.id}
+          value={topic}
+          handleClick={() => props.handleClick(topic.id)}
+        />
+        <TopicDetails value={topic} openId={props.openId} />
       </div>
     );
   });
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -4,20 +4,23 @@ import Accordion from "./accordion";
 
 const listOfTopics = [
   {
+    id: 1,
     name: "Hypertext Markup Language",
     details:
       "HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.",
   },
   {
+    id: 2,
     name: "Cascading Style Sheets",
     details:
       "CSS is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.[1] CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.[2]",
   },
   {
+    id: 3,
     name: "JavaScript",
     details:
       "JavaScript often abbreviated as JS, is a programming language that conforms to the ECMAScript specification.[7] JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.",
-  },
+  }
 ];
 
 ReactDOM.render(
