5550447e282a32a0a4e8eed0586924a40bdcd606
shawnkost <shawnmkost@gmail.com>
2021-01-21 18:11:06 -0800

    Started working on timeOut functionality

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -3,17 +3,60 @@ import React from "react";
 export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
-    console.log(props)
+    this.state = { currentPhotoId: 1 };
+    this.previousPhoto = this.previousPhoto.bind(this);
+    this.nextPhoto = this.nextPhoto.bind(this);
+  }
+  previousPhoto() {
+    if (this.state.currentPhotoId === 1) {
+      this.setState({ currentPhotoId: 5 });
+    } else {
+      this.setState({ currentPhotoId: this.state.currentPhotoId - 1 });
+    }
+  }
+  nextPhoto() {
+    if (this.state.currentPhotoId === 5) {
+      this.setState({ currentPhotoId: 1 });
+    } else {
+      this.setState({ currentPhotoId: this.state.currentPhotoId + 1 });
+    }
+    this.timeOut()
+  }
+  timeOut() {
+    const time = setTimeout(this.nextPhoto, 3000);
   }
   render() {
-    // const images = props.images;
     return (
-      <div>
-        <i className="fas fa-arrow-left"></i>
-        <img src={ this.props.images[0].src }></img>
-        <i className="fas fa-arrow-right"></i>
+      <div className="container">
+        <i className="fas fa-arrow-left" onClick={ this.previousPhoto }></i>
+        <Images
+          images={ this.props.images }
+          currentPhotoId={ this.state.currentPhotoId }
+        />
+        <i className="fas fa-arrow-right" onClick={ this.nextPhoto }></i>
       </div>
     );
   }
+}
+
+function CurrentImage(props) {
+  if (props.currentPhotoId === props.value.id) {
+    return <img classname="show" src={props.value.src}></img>;
+  } else {
+    return <img className="hide"></img>;
+  }
+}
+
+function Images(props) {
+  const images = props.images;
+  const newImages = images.map((image) => {
+    return (
+      <CurrentImage
+        key={image.id}
+        value={image}
+        currentPhotoId={props.currentPhotoId}
+      />
+    );
+  });
+  return newImages;
 }
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -3,26 +3,11 @@ import ReactDOM from "react-dom";
 import Carousel from "./carousel";
 
 const images = [
-  {
-    name: "Bulbasaur",
-    src: "../images/1.png"
-  },
-  {
-    name: "Charmander",
-    src: "../images/2.png"
-  },
-  {
-    name: "Squirtle",
-    src: "../images/3.png"
-  },
-  {
-    name: "Pikachu",
-    src: "../images/4.png"
-  },
-  {
-    name: "Jigglypuff",
-    src: "../images/5.png"
-  },
+  { id: 1, name: "Bulbasaur", src: "../images/1.png" },
+  { id: 2, name: "Charmander", src: "../images/2.png" },
+  { id: 3, name: "Squirtle", src: "../images/3.png" },
+  { id: 4, name: "Pikachu", src: "../images/4.png" },
+  { id: 5, name: "Jigglypuff", src: "../images/5.png" },
 ];
 
 ReactDOM.render(<Carousel images={images} />, document.querySelector("#root"));
