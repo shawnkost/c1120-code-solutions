5ee9843cae026a0c0ecb1ff8dcd4ebb68d52543c
shawnkost <shawnmkost@gmail.com>
2021-01-25 18:14:21 -0800

    Started working on feedback

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -3,7 +3,8 @@ import React from "react";
 export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { currentPhotoId: 1, timeoutId: null };
+    this.state = { currentPhotoId: 1 };
+    this.timeoutId = null;
     this.previousPhoto = this.previousPhoto.bind(this);
     this.nextPhoto = this.nextPhoto.bind(this);
     this.timeOut = this.timeOut.bind(this);
@@ -16,7 +17,7 @@ export default class Carousel extends React.Component {
     } else {
       this.setState({ currentPhotoId: this.state.currentPhotoId - 1 });
     }
-    clearTimeout(this.state.timeoutId);
+    clearTimeout(this.timeoutId);
     this.timeOut();
   }
   nextPhoto() {
@@ -25,32 +26,38 @@ export default class Carousel extends React.Component {
     } else {
       this.setState({ currentPhotoId: this.state.currentPhotoId + 1 });
     }
-    clearTimeout(this.state.timeoutId);
+    clearTimeout(this.timeoutId);
     this.timeOut();
   }
   specificPhoto(id) {
-    clearTimeout(this.state.timeoutId);
+    clearTimeout(this.timeoutId);
     this.setState({ currentPhotoId: id });
     this.timeOut();
   }
   timeOut() {
     const time = setTimeout(this.nextPhoto, 3000);
-    this.setState({ timeoutId: time });
+    this.timeoutId = time;
   }
   render() {
     return (
       <div>
         <div className="container">
-          <i className="fas fa-arrow-left" onClick={this.previousPhoto}></i>
+          <i
+            className="fas fa-arrow-left arrow-left"
+            onClick={this.previousPhoto}
+          ></i>
           <Images
             images={this.props.images}
             currentPhotoId={this.state.currentPhotoId}
           />
-          <i className="fas fa-arrow-right" onClick={this.nextPhoto}></i>
+          <i
+            className="fas fa-arrow-right arrow-right"
+            onClick={this.nextPhoto}
+          ></i>
         </div>
         <div className="circles">
           <Circles
-            circles={this.props.circles}
+            images={this.props.images}
             currentPhotoId={this.state.currentPhotoId}
             specificPhoto={this.specificPhoto}
           />
@@ -61,12 +68,13 @@ export default class Carousel extends React.Component {
 }
 
 const CurrentImage = (props) => {
-  if (props.currentPhotoId === props.value.id) {
-    return <img className="show" src={props.value.src}></img>;
-  } else {
-    return <img className="hide"></img>;
-  }
-}
+  return (
+    <img
+      className={props.currentPhotoId === props.value.id ? "show" : "hide"}
+      src={props.value.src}
+    ></img>
+  );
+};
 
 const Images = (props) => {
   const images = props.images;
@@ -80,27 +88,32 @@ const Images = (props) => {
     );
   });
   return newImages;
-}
+};
 
 const CurrentCircle = (props) => {
-  if (props.currentPhotoId === props.value.id) {
-    return <i className="fas fa-circle" onClick={props.specificPhoto}></i>;
-  } else {
-    return <i className="far fa-circle" onClick={props.specificPhoto}></i>;
-  }
-}
+  return (
+    <i
+      className={
+        props.currentPhotoId === props.value.id
+          ? "fas fa-circle circle"
+          : "far fa-circle circle"
+      }
+      onClick={props.specificPhoto}
+    ></i>
+  );
+};
 
 const Circles = (props) => {
-  const circles = props.circles;
-  const newCircles = circles.map((circle) => {
+  const images = props.images;
+  const newImages = images.map((image) => {
     return (
       <CurrentCircle
-        key={circle.id}
-        value={circle}
+        key={image.id}
+        value={image}
         currentPhotoId={props.currentPhotoId}
-        specificPhoto={() => props.specificPhoto(circle.id)}
+        specificPhoto={() => props.specificPhoto(image.id)}
       />
     );
   });
-  return newCircles;
-}
+  return newImages;
+};
